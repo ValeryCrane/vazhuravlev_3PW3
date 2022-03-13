488f18d16ea282a0070de64f16a5dd43aa61f9d0
Valery Crane <zhulery@gmail.com>
2022-03-13 13:00:28 +0500

    Initial blank project

---
+++ vazhuravlev_3PW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vazhuravlev_3PW3
+//
+//  Created by Валерий Журавлев on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
